755b587b8d8f47db408fc2ce333eb4af9dbbc0ee
MarFerPra <marco.fp94@gmail.com>
2016-01-27 12:27:21 +0100

    User model & login functionalities

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -9,6 +9,8 @@
 #  updated_at :datetime         not null
 #
 
+require 'digest'
+
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 
@@ -20,9 +22,35 @@ class User < ActiveRecord::Base
                     :format => {:with => email_regex},
                     :uniqueness => { :case_sensitive => false }
 
-                    # Aqui se genera el atributo virtual password_confirmation (true) 
+                    # Aqui se genera el atributo virtual password_confirmation (true)
   validates :password, :presence => true,
                        :confirmation => true,
                        :length => {:within => 6..20}
+
+
+ before_save :encrypt_password
+
+ # True if submitted password matches user's hashed password
+ def has_password?(submitted_password)
+   self.encrypted_password == encrypt(submitted_password)
+ end
+
+ private
+  def encrypt_password
+    self.salt = make_salt if new_record?
+    self.encryped_password = encrypt(self.password)
+  end
+
+  def encrypt(string)
+    secure_hash("#{salt}--#{string}")
+  end
+
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{self.password}")
+  end
+
+  def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+  end
 
 end
