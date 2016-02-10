0e8bafa78255c89483db90cc4eacd5eca486ee19
MarFerPra <marco.fp94@gmail.com>
2016-02-10 09:49:20 +0100

    Fixed session issue

--- app/concepts/users/operation.rb
+++ app/concepts/users/operation.rb
@@ -57,10 +57,14 @@ end
 class User::Authenticate < Trailblazer::Operation
   attr_accessor :user
 
+# I could probably improve this
   def process(params)
     email, submitted_password = params[:session][:email], params[:session][:password]
     @user = User.find_by_email(email)
     return nil if @user.nil?
-    return @user if @user.has_password?(submitted_password)
+
+    if !@user.has_password?(submitted_password)
+      @user = nil
+    end
   end
 end
