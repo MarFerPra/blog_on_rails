33a6101bd5719c6c67fdcc976250eaa6ac4dcd84
MarFerPra <marco.fp94@gmail.com>
2016-02-19 12:58:56 +0100

    React is working, and I'm pretty sure im not dreaming.

--- app/assets/javascripts/components/commentsbox.js.jsx
+++ app/assets/javascripts/components/commentsbox.js.jsx
@@ -3,12 +3,12 @@ var Comment = React.createClass({
   render: function(){
     return (
       <div className="Comment">
-        <p> <i>
-          TEXTO: {this.props.text}
+        <p><i>
+          {this.props.text}
         </i></p>
-        <h4>
-          AUTOR: {this.props.author}
-        </h4>
+        <p>
+          - {this.props.author}
+        </p>
       </div>
     );
   }
@@ -46,15 +46,64 @@ var CommentsBox = React.createClass({
   render: function() {
     return (
       <div className="CommentsBox">
-        <h2> Comments </h2>
-        <h3> Url is: {this.state.url} </h3>
+        <h5><strong> Comments: </strong></h5>
         <CommentList data={this.state.data} />
+        <CommentForm current_user={this.props.current_user} />
       </div>
     );
   }
 });
 
-/* TODO :Not printing anything -> Polque zeño? polque? */
+var CommentForm = React.createClass({
+  getInitialState: function(){
+    return {author:'', text:''};
+  },
+  handleAuthorChange: function(e){
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e){
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e){
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if(!text  || !author){
+      return;
+    }
+    this.props.onCommentSubmit({author: author, text: text});
+    this.setState({author:'', text:''});
+  },
+  render: function(){
+    return(
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <p>
+          <input
+            type="text"
+            placeholder="Your name"
+            class="form-control commenter_name"
+            value={this.state.author}
+            onChange={this.handleAuthorChange}
+          />
+        </p>
+        <p>
+          <input
+            type="text"
+            placeholder="Whatever you have to say"
+            class="form-control comment_text"
+            value={this.state.text}
+            onChange={this.handleTextChange}
+          />
+        </p>
+        <p>
+          <input type="submit" value="Post comment"
+            class="btn btn-info btn-default btn-block"/>
+        </p>
+      </form>
+    );
+  }
+});
+
 var CommentList = React.createClass({
   render: function(){
     var commentNodes = this.props.data.map(function(comment){
